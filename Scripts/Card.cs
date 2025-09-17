bfd30468403e343d857f3fda882c66c6e97bc3bf
Parallax144 <2kkfahh@gmail.com>
2025-09-17 19:35:29 -0300

    bug do tooltip resolvido
    resolvido o bug que acontecia do tooltip não mostrar ao passar o mouse por cima

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -5,6 +5,8 @@ public partial class Card : BaseCard
 {
     public CardData Data { get; private set; }
 
+    private PanelContainer tooltip;
+
     public void SetCard(CardData data, Texture2D texture)
     {
         Data = data;
@@ -19,5 +21,69 @@ public partial class Card : BaseCard
             Rank.Ten or Rank.Jack or Rank.Queen or Rank.King => 10,
             _ => (int)Data.Rank
         };
+    }
+
+    private void OnMouseEntered()
+    {
+        ShowTooltip();
+    }
+
+    private void OnMouseExited()
+    {
+        HideTooltip();
+    }
+
+    private void ShowTooltip()
+    {
+        if (tooltip != null) return;
+
+        // Caixa branca
+        tooltip = new PanelContainer
+        {
+            Modulate = new Color(255, 255, 255, 1), // branco sólido
+            SizeFlagsHorizontal = Control.SizeFlags.ShrinkCenter,
+            SizeFlagsVertical = Control.SizeFlags.ShrinkCenter
+        };
+
+        tooltip.AddThemeConstantOverride("margin_left", 6);
+        tooltip.AddThemeConstantOverride("margin_right", 6);
+        tooltip.AddThemeConstantOverride("margin_top", 4);
+        tooltip.AddThemeConstantOverride("margin_bottom", 4);
+
+        var vbox = new VBoxContainer
+        {
+            CustomMinimumSize = new Vector2(120, 0)
+        };
+        vbox.AddThemeConstantOverride("separation", 4);
+
+        // Letras pretas usando Modulate
+        var nameLabel = new Label { Text = $"Nome: {Data.Name}" };
+        nameLabel.Modulate = Colors.Black;
+
+        var chipLabel = new Label { Text = $"Chips: {GetChipValue()}" };
+        chipLabel.Modulate = Colors.Black;
+
+        vbox.AddChild(nameLabel);
+        vbox.AddChild(chipLabel);
+
+        tooltip.AddChild(vbox);
+        AddChild(tooltip);
+
+        tooltip.CallDeferred("set_position", new Vector2(0, -tooltip.Size.Y - 10));
+    }
+
+
+
+    private void HideTooltip()
+    {
+        tooltip?.QueueFree();
+        tooltip = null;
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        MouseEntered += OnMouseEntered;
+        MouseExited += OnMouseExited;
     }
 }
